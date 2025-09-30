11c3fd432131ebadf69c1dba3521970361b5fe0f
AgustinBertoliniFlyDevs <agustin.bertolini>
2025-09-30 19:36:12 -0300

    Sumo creditos

---
+++ lib/Creditos.py
@@ -0,0 +1,8 @@
+INFORMACION_JUEGO = "Aqua Survivor - Minijuego educativo sobre el uso responsable del agua."
+AUTORES = [
+    "Tomas Segade"
+    "Marcos Dermi",
+    "Joel Damerdjian",
+    "Ignacio Rodriguez",
+    "Agustin Bertolini",
+]
